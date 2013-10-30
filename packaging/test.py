b67ef8e09dc9e15249de1daa13e4c625a6d56f6c
James Tanner <tanner.jc@gmail.com>
2013-10-29 20:41:25 -0400

    add some untracked files and a basic package test

---
+++ packaging/test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+"""
+The remote_user for a connection has an order of preference based on various sources ...
+
+inventory ansible_ssh_user > task remote_user > play remote_user > playbook remote_user > cli --user
+"""
+
+import unittest
+import os 
+import sys
+import subprocess 
+import shlex
+import tempfile
+
+
+# MAKE SURE hacking/env-setup IS SOURCED BEFORE RUNNING TESTS
+def test_ansible_playbook_in_path():
+    path = None
+    try:
+        path = subprocess.check_output(shlex.split('which ansible-playbook'))
+    except:
+        pass
+
+    assert path is not None
+
+# MAKE SURE A SINGLE PACKAGE CAN BE INSTALLED
+def test_basic_package_install():
+
+    #<el6.lab.net> REMOTE_MODULE yum name=dos2unix state=installed
+    #ok: [el6.lab.net] => {"changed": false, "msg": "", "rc": 0, "results": 
+    #    ["dos2unix-3.1-37.el6.x86_64 providing dos2unix is already installed"]}
+
+    output = None
+    cmdargs = "ansible-playbook -c ssh -vvvv -i inventory -t one yum.yml"
+    cmdargs = shlex.split(cmdargs)
+    try:
+        #output = subprocess.check_output(cmdargs, stderr=fh, stdout=fh2)
+        output = subprocess.check_output(cmdargs)
+    except:
+        pass
+
+    assert output is not None
+    #output = output.strip()
+    lines = output.split("\n")
+
+
+    installed = []
+
+    for line in lines:
+        # grep out package names sent to the yum command
+        if 'REMOTE_MODULE yum' in line:
+            words = shlex.split(line)
+            for word in words:
+                if word.startswith("name="):
+                    packages = shlex.split(word, "=")
+                    packages = packages[0].replace('name=', '').split(',')
+                    for p in packages:
+                        installed.append(p)
+        # get the result
+        if '"results":' in line:
+            pass
+
+    assert len(installed) == 1, "%s should have one package" % installed
