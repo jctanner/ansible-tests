8eab6fa3d4b739b7a2cebe8f56a244c37c8eea7e
James Tanner <tanner.jc@gmail.com>
2013-10-29 23:11:22 -0400

    fix test name

--- packaging/test.py
+++ packaging/test.py
@@ -87,6 +87,35 @@ def test_package_list_install():
                 if word.startswith("name="):
                     packages = shlex.split(word, "=")
                     packages = packages[0].replace('name=', '').split(',')
-                    open("/tmp/awx.log", "a").write("%s\n" % packages)
+                    #open("/tmp/awx.log", "a").write("%s\n" % packages)
                     assert len(packages) > 1, "%s should have been more than one package name" % packages
+
+
+# MAKE SURE ONE PACKAGE IS SENT IF TASK IS CONDITIONAL
+def test_conditional_package_list_install():
+
+    output = None
+    cmdargs = "ansible-playbook -c ssh -vvvv -i inventory -t conditional_list yum.yml"
+    cmdargs = shlex.split(cmdargs)
+    try:
+        #output = subprocess.check_output(cmdargs, stderr=fh, stdout=fh2)
+        output = subprocess.check_output(cmdargs)
+    except:
+        pass
+
+    assert output is not None
+    lines = output.split("\n")
+
+    for line in lines:
+        # grep out package names sent to the yum command
+        if 'REMOTE_MODULE yum' in line:
+            words = shlex.split(line)
+            for word in words:
+                if word.startswith("name="):
+                    packages = shlex.split(word, "=")
+                    packages = packages[0].replace('name=', '').split(',')
+                    #open("/tmp/awx.log", "a").write("%s\n" % packages)
+                    assert len(packages) == 1, "%s should only contain one packagename" % packages
+                    assert packages[0] == "strace", "%s should be 'strace'"  % packages
+                    assert packages[0] != "dos2unix", "should have skipped dos2unix: %s"  % packages
 
